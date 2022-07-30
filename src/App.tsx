ea876e72cda1729a1c8c9ecb286cb254efef9f3e
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-07-30 07:42:08 -0400

    :children_crossing: added app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import {
+  AppContainer,
+  ColumnContainer,
+  ColumnTitle,
+  CardContainer,
+} from "./assets/styles/styles";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <AppContainer>
+        <ColumnContainer>
+          <ColumnTitle>World of Walker</ColumnTitle>
+          <CardContainer>Alan Walker, Torine</CardContainer>
+        </ColumnContainer>
+      </AppContainer>
+    </div>
+  );
 }
 
 export default App;
