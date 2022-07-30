12c5bf0949add487c28b9d29581c62a7c32058e6
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-07-30 13:04:23 -0400

    :sparkles: added input ref

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -4,16 +4,21 @@ import {
   NewItemButton,
   NewItemInput,
 } from "../assets/styles/styles";
-
+import { useFocus } from "../utils/useFocus";
 interface NewItemFormProps {
   onAdd(text: string): void;
 }
 
 function NewItemForm({ onAdd }: NewItemFormProps) {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
